1961766f9caf87d57cd653f29638664434db6fc0
Angela <arivas@cetys.edu.mx>
2026-03-04 22:17:10 -0800

    cambios andrea

--- server/index.js
+++ server/index.js
@@ -5,16 +5,36 @@ const io = require('socket.io')(http, {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
 
     socket.on('message', (data) => {
-        console.log(data);
+
+        if (!socket.nickname) {
+            socket.nickname = data.nickname;
+
+            io.emit('message', {
+                nickname: "Sistema",
+                text: `${socket.nickname} se conectó`
+            });
+        }
 
         io.emit('message', {
             nickname: data.nickname,
             text: data.text
         });
+
     });
+
+    socket.on('disconnect', () => {
+
+        if (socket.nickname) {
+            io.emit('message', {
+                nickname: "Sistema",
+                text: `${socket.nickname} se desconectó`
+            });
+        }
+
+    });
+
 });
 
 http.listen(8080, () => console.log('listening on http://localhost:8080'));
